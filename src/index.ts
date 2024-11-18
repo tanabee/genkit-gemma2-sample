71183b4bbe72efa761036315a26b5931f8090af3
tanabee <tanabee1986@gmail.com>
2024-11-18 09:39:07 +0900

    Update to support Genkit 0.9

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,20 @@
-import * as z from 'zod'
-import { generate } from '@genkit-ai/ai'
-import { configureGenkit } from '@genkit-ai/core'
-import { defineFlow, startFlowsServer } from '@genkit-ai/flow'
+import { genkit, z } from 'genkit'
 import { ollama } from 'genkitx-ollama'
 
-configureGenkit({
-  plugins: [
-    ollama({
-      models: [{ name: 'gemma2' }],
-      serverAddress: 'http://127.0.0.1:11434',
-    }),
-  ],
-  logLevel: 'debug',
-  enableTracingAndMetrics: true,
+const ai = genkit({
+  plugins: [ollama({ models: [{ name: 'gemma2' }], serverAddress: 'http://127.0.0.1:11434' })],
+  model: 'ollama/gemma2',
 })
 
-export const mainFlow = defineFlow(
+const mainFlow = ai.defineFlow(
   {
     name: 'mainFlow',
     inputSchema: z.string(),
-    outputSchema: z.string(),
   },
-  async prompt => {
-    const llmResponse = await generate({
-      prompt,
-      model: 'ollama/gemma2',
-    })
-    return llmResponse.text()
+  async input => {
+    const { text } = await ai.generate(input)
+    return text
   }
 )
 
-startFlowsServer()
+ai.startFlowServer({ flows: [mainFlow] })
